1230d228adfd497067ebf13dc12f7e0f88ef6fa6
Olivia <guesso.olivia@gmail.com>
2022-05-10 01:09:24 -0500

    Added variables to general fucntions

--- demo.sql
+++ demo.sql
@@ -1,3 +1,17 @@
+-- Function 8: Create User --
+INSERT IGNORE INTO ACCOUNT
+	VALUES("user", "password");
+
+-- Function 3: Create User List --
+INSERT INTO ACCOUNT_LIST
+	VALUES("1", "Favorites", "This is a list of my all time favorite movies.", CURDATE(), NULL);
+INSERT INTO CURATES
+	VALUES("user", "1");
+
+-- Function 4: Add To List --
+INSERT IGNORE INTO IN_LIST
+	VALUES ((SELECT ACCOUNT_LIST.list_id FROM ACCOUNT_LIST, ACCOUNT, CURATES WHERE ACCOUNT.username = "user" AND CURATES.username = "user" AND CURATES.list_id = ACCOUNT_LIST.list_id AND ACCOUNT_LIST.name = "Favorites"), (SELECT movie_id FROM MOVIE WHERE title = "La La Land"), "La La Land");
+
 -- Function 2: Search Movie --
 SELECT title, COUNT(title) AS "Number of Results"
 FROM MOVIE, PRODUCTION_COMPANY
@@ -9,3 +23,10 @@ OR (MOVIE.movie_id IN (SELECT movie_id
                  		WHERE PERSON.name LIKE "Emma Stone" 
                  		AND PERSON.person_id = INVOLVED_WITH.person_id))
 GROUP BY title;
+
+-- Find all movies person is involved with --
+SELECT title FROM MOVIE
+WHERE movie_id IN (SELECT movie_id FROM INVOLVED_WITH
+                   WHERE INVOLVED_WITH.movie_id = MOVIE.movie_id
+                   AND person_id IN (SELECT person_id FROM PERSON
+                                     WHERE name = "Emma Stone"));
--- functions.sql
+++ functions.sql
@@ -7,37 +7,44 @@
 -- Function 1: Get Movie Info --
 SELECT title, release_date, gross_revenue, genre, award_show, award_name, award_year, company_name, PERSON.name
 FROM MOVIE, AWARD, MOVIE_GENRE, PRODUCTION_COMPANY, PERSON
-WHERE MOVIE.movie_id = "001" 
+WHERE MOVIE.movie_id = @id
 	OR (person_id IN (SELECT person_id
                   		FROM INVOLVED_WITH
-                  		WHERE movie_id = "001"))
+                  		WHERE movie_id = @id))
 ORDER BY genre, award_show, company_name, PERSON.name;
 
 -- Function 2: Search Movie --
 SELECT title, COUNT(title) AS "Number of Results"
 FROM MOVIE, PRODUCTION_COMPANY
-WHERE MOVIE.title LIKE "Clue"
-OR MOVIE.release_date LIKE "2018-05-07"
-OR PRODUCTION_COMPANY.company_name LIKE "Productions"
+WHERE MOVIE.title LIKE @title
+OR MOVIE.release_date LIKE @date
+OR PRODUCTION_COMPANY.company_name LIKE @company
 OR (MOVIE.movie_id IN (SELECT movie_id
                  		FROM INVOLVED_WITH, PERSON
-                 		WHERE PERSON.name LIKE "Emma Stone" 
+                 		WHERE PERSON.name LIKE @personName
                  		AND PERSON.person_id = INVOLVED_WITH.person_id))
 GROUP BY title;
 
 -- Function 3: Create User List --
 INSERT INTO ACCOUNT_LIST
-	VALUES("1", "Favorites", "This is a list of my all time favorite movies.", CURDATE(), NULL);
+	VALUES(@listNum, @name, @description, CURDATE(), NULL);
 INSERT INTO CURATES
-	VALUES("user", "1");
+	VALUES(@username, @listNum);
 
 -- Function 4: Add To List --
 INSERT IGNORE INTO IN_LIST
-	VALUES ((SELECT ACCOUNT_LIST.list_id FROM ACCOUNT_LIST, ACCOUNT, CURATES WHERE ACCOUNT.username = "user" AND CURATES.username = "user" AND CURATES.list_id = ACCOUNT_LIST.list_id AND ACCOUNT_LIST.name = "Favorites"), (SELECT movie_id FROM MOVIE WHERE title = "La La Land"), "La La Land");
+	VALUES ((SELECT ACCOUNT_LIST.list_id FROM ACCOUNT_LIST, ACCOUNT, CURATES WHERE ACCOUNT.username = @username AND CURATES.username = @username AND CURATES.list_id = ACCOUNT_LIST.list_id AND ACCOUNT_LIST.name = @listName), (SELECT movie_id FROM MOVIE WHERE title = @movie), @movie);
 
 -- Function 8: Create User --
 INSERT IGNORE INTO ACCOUNT
-	VALUES("user", "password");
+	VALUES(@user, @password);
+
+-- Find all movies person is involved with --
+SELECT title FROM MOVIE
+WHERE movie_id IN (SELECT movie_id FROM INVOLVED_WITH
+                   WHERE INVOLVED_WITH.movie_id = MOVIE.movie_id
+                   AND person_id IN (SELECT person_id FROM PERSON
+                                     WHERE name = @personName));
 
 -- ----------------------- --
 
